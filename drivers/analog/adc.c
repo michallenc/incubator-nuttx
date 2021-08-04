d3a4816fa04aefc08091a6a71e9e47a932815a58
Michal Lenc <michallenc@seznam.cz>
2021-08-04 16:03:57 +0200

    adc: add IOCTL ANIOC_SAMPLES_ON_READ to return number of samples waiting to be read

--- drivers/analog/adc.c
+++ drivers/analog/adc.c
@@ -80,6 +80,7 @@ static void    adc_notify(FAR struct adc_dev_s *dev);
 static int     adc_poll(FAR struct file *filep, struct pollfd *fds,
                         bool setup);
 static int     adc_reset_fifo(FAR struct adc_dev_s *dev);
+static int     adc_samples_on_read(FAR struct adc_dev_s *dev);
 
 /****************************************************************************
  * Private Data
@@ -441,6 +442,12 @@ static int adc_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
         }
         break;
 
+      case ANIOC_SAMPLES_ON_READ:
+        {
+          ret = adc_samples_on_read(dev);
+        }
+        break;
+
       default:
         {
           /* Those IOCTLs might be used in arch specific section */
@@ -632,7 +639,7 @@ return_with_irqdisabled:
 }
 
 /****************************************************************************
- * Name: adc_clear_fifo
+ * Name: adc_reset_fifo
  ****************************************************************************/
 
 static int adc_reset_fifo(FAR struct adc_dev_s *dev)
@@ -644,12 +651,39 @@ static int adc_reset_fifo(FAR struct adc_dev_s *dev)
 
   flags = enter_critical_section();
 
-  fifo->af_tail = 0;
-  fifo->af_head = 0;
+  fifo->af_head = fifo->af_tail;
 
   leave_critical_section(flags);
 
   return OK;
+}
+
+/****************************************************************************
+ * Name: adc_samples_on_read
+ ****************************************************************************/
+
+static int adc_samples_on_read(FAR struct adc_dev_s *dev)
+{
+  irqstate_t flags;
+  FAR struct adc_fifo_s *fifo = &dev->ad_recv;
+  int16_t ret;
+
+  /* Interrupts must be disabled while accessing the ad_recv FIFO */
+
+  flags = enter_critical_section();
+
+  ret = fifo->af_tail - fifo->af_head;
+
+  leave_critical_section(flags);
+
+  if (ret < 0)
+    {
+      /* Increment return value by the size of FIFO */
+
+      ret += CONFIG_ADC_FIFOSIZE;
+    }
+
+  return ret;
 }
 
 /****************************************************************************
